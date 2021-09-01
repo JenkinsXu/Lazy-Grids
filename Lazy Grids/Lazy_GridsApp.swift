e19b35ccd319fca9abf5d5f175f0025019062576
Yongqi Xu <chewing_cake@icloud.com>
2021-09-01 11:27:01 -0700

    Initial Commit

---
+++ Lazy Grids/Lazy_GridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lazy_GridsApp.swift
+//  Lazy Grids
+//
+//  Created by Yongqi Xu on 2021-09-01.
+//
+
+import SwiftUI
+
+@main
+struct Lazy_GridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
